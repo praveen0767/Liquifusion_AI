65291d04ee369669beec6de26a2cc9d3fe90f1b7
praveen0767 <143109797+praveen0767@users.noreply.github.com>
2025-07-27 10:23:23 +0530

    Update generate_did.sh

--- generate_did.sh
+++ generate_did.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
-function generate_did() {
-    local canister=$1
-    canister_root="src/$canister"
-
-    # Build the canister
-    cargo build --manifest-path="$canister_root/Cargo.toml" \
-        --target wasm32-unknown-unknown \
-        --release --package "$canister"
-
-    # Extract the Candid interface using ic-wasm
-    ic-wasm "target/wasm32-unknown-unknown/release/$canister.wasm" --extract-candid \
-        > "$canister_root/$canister.did"
-}
-
-CANISTERS="freelance_backend"
-
-for canister in $(echo $CANISTERS | sed "s/,/ /g")
-do
-    generate_did "$canister"
-done
+# Generate Candid interface for backend canister
+cd src/liquifusion_backend
+dfx canister create liquifusion_backend --no-wallet
+dfx build liquifusion_backend
+dfx canister id liquifusion_backend > ../canister_ids.json
+dfx generate liquifusion_backend --did > liquifusion_backend.did
+echo "Generated DID and updated canister_ids.json"
